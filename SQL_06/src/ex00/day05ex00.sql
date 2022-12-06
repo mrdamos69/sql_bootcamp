209db4560123ebd2609f78599f170e4ef465b5af
Dmitrii Chich <86652230+mrdamos69@users.noreply.github.com>
2022-12-06 19:33:00 +0300

    update start project / 00

---
+++ SQL_06/src/ex00/day05ex00.sql
@@ -0,0 +1,4 @@
+create table person_discounts (id bigint primary key , person_id bigint not null ,
+pizzeria_id bigint not null, discount numeric not null,
+constraint fk_person_visits_person_id foreign key (person_id) references person(id),
+constraint fk_person_visits_pizzeria_id foreign key  (pizzeria_id) references pizzeria(id));
