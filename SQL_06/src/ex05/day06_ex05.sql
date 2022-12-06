002ad0065473a4aed31fcb837d138c89fffa94da
Dmitrii Chich <86652230+mrdamos69@users.noreply.github.com>
2022-12-07 00:52:06 +0300

    Update day06_ex05.sql

---
+++ SQL_06/src/ex05/day06_ex05.sql
@@ -0,0 +1,5 @@
+comment on table person_discounts is 'Таблица для персональных скидок клиентам';
+comment on column person_discounts.id is 'Индификационный номер для каждой из строк';
+comment on column person_discounts.person_id is 'Индификационный номер для клиента';
+comment on column person_discounts.pizzeria_id is 'Индификационный номер для пиццы';
+comment on column person_discounts.discount is 'Персональная скидка для клиента';
