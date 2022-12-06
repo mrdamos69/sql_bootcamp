c8dd3bd76e746b5a080c7dd1f7ea62a6c0b9fcee
Dmitrii Chich <86652230+mrdamos69@users.noreply.github.com>
2022-12-06 23:26:44 +0300

    up 06 day / add  03

---
+++ SQL_06/src/ex03/day06_ex03.sql
@@ -0,0 +1,4 @@
+set enable_bitmapscan  = off;
+set enable_seqscan = off;
+create unique index idx_person_discounts_unique on person_discounts (person_id, pizzeria_id);
+explain analyze select person_id, pizzeria_id from person_discounts
