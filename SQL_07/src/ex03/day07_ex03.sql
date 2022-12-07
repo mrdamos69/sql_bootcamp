34bf33b98d56bf1790a50a4c5c0afbdc42996556
Dmitrii Chich <86652230+mrdamos69@users.noreply.github.com>
2022-12-08 01:07:32 +0300

    Update day07_ex03.sql

---
+++ SQL_07/src/ex03/day07_ex03.sql
@@ -0,0 +1,13 @@
+with top_order as (select pz.name, count(m.pizzeria_id), concat('order') as action_type from person_order as po
+					join menu as m on po.menu_id = m.id
+					join pizzeria as pz on m.pizzeria_id = pz.id
+					group by pz.name
+					order by action_type, count DESC),
+	top_visit as (select pz.name, count(pv.pizzeria_id), concat('visit') as action_type from person_visits as pv
+					join pizzeria as pz on pz.id = pv.pizzeria_id
+					group by pz.name
+					order by action_type, count DESC)
+					
+select top_visit.name, (top_order.count + top_visit.count) as total_count
+from top_order
+join top_visit on top_visit.name = top_order.name
