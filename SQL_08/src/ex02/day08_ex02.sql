f572821e0d7e2bcb17233c88c14695e6d07d66e2
Dmitrii Chich <86652230+mrdamos69@users.noreply.github.com>
2022-12-09 00:55:17 +0300

    update day08_ex02

---
+++ SQL_08/src/ex02/day08_ex02.sql
@@ -0,0 +1,17 @@
+--Session_1
+
+begin transaction isolation level repeatable read;
+begin transaction;
+select * from pizzeria where name = 'Pizza Hut';
+update pizzeria set rating = 4 where name = 'Pizza Hut';
+commit;
+select * from pizzeria where name = 'Pizza Hut';
+
+--Session_2
+
+begin transaction isolation level repeatable read;
+begin transaction;
+select * from pizzeria where name = 'Pizza Hut';
+update pizzeria set rating = 3.6 where name = 'Pizza Hut';
+commit;
+select * from pizzeria where name = 'Pizza Hut';
