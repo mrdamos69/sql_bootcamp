4dc4ccfa7ad773260b917d138363616baf56d119
Dmitrii Chich <86652230+mrdamos69@users.noreply.github.com>
2022-12-09 01:24:06 +0300

    update day08_ex04

---
+++ SQL_08/src/ex04/day08_ex04.sql
@@ -0,0 +1,17 @@
+--Session_1
+begin transaction ISOLATION LEVEL SERIALIZABLE;
+--Session_2
+begin transaction ISOLATION LEVEL SERIALIZABLE;
+
+--Session_1
+select * from pizzeria where name = 'Pizza Hut';
+--Session_2
+update pizzeria set rating = 3.6 where name = 'Pizza Hut';
+commit;
+
+--Session_1
+select * from pizzeria where name = 'Pizza Hut';
+commit;
+select * from pizzeria where name = 'Pizza Hut';
+--Session_2
+select * from pizzeria where name = 'Pizza Hut';
